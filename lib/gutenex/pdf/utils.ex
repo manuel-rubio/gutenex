09a17dada273e089685176b5a14820430624675f
tulinmola <tulinmola@gmail.com>
2020-01-16 14:35:19 +0100

    Fix string escaping

--- lib/gutenex/pdf/utils.ex
+++ lib/gutenex/pdf/utils.ex
@@ -1,26 +1,8 @@
 defmodule Gutenex.PDF.Utils do
-  # Base case, escaping the empty string is the empty string
-  def escape(<<>>) do
-    <<>>
-  end
-
-  # escape left parenthesis
-  def escape(<<"(", rest::binary>>) do
-    "\\(" <> escape(rest)
-  end
-
-  # escape right parenthesis
-  def escape(<<")", rest::binary>>) do
-    "\\)" <> escape(rest)
-  end
-
-  # escape backslash
-  def escape(<<"\\", rest::binary>>) do
-    "\\\\" <> escape(rest)
-  end
-
-  # Head doesn't need escaping, escape the rest
-  def escape(<<first_byte::binary-size(1), rest::binary>>) do
-    first_byte <> escape(rest)
+  def escape(string) do
+    string
+    |> String.replace("(", "\\(")
+    |> String.replace(")", "\\)")
+    |> String.replace("||", "\\\\")
   end
 end
