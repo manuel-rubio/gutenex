deac84c713065d34ca61ee3d69f25027c88d3725
Manuel Rubio <manuel@altenwald.com>
2020-02-11 00:47:31 +0100

    Revert "Fix string escaping"
    This reverts commit 09a17dada273e089685176b5a14820430624675f.

--- lib/gutenex/pdf/utils.ex
+++ lib/gutenex/pdf/utils.ex
@@ -1,8 +1,26 @@
 defmodule Gutenex.PDF.Utils do
-  def escape(string) do
-    string
-    |> String.replace("(", "\\(")
-    |> String.replace(")", "\\)")
-    |> String.replace("||", "\\\\")
+  # Base case, escaping the empty string is the empty string
+  def escape(<<>>) do
+    <<>>
+  end
+
+  # escape left parenthesis
+  def escape(<<"(", rest::binary>>) do
+    "\\(" <> escape(rest)
+  end
+
+  # escape right parenthesis
+  def escape(<<")", rest::binary>>) do
+    "\\)" <> escape(rest)
+  end
+
+  # escape backslash
+  def escape(<<"\\", rest::binary>>) do
+    "\\\\" <> escape(rest)
+  end
+
+  # Head doesn't need escaping, escape the rest
+  def escape(<<first_byte::binary-size(1), rest::binary>>) do
+    first_byte <> escape(rest)
   end
 end
